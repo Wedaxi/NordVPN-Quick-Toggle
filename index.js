93647c08a20188aa0f9b7a34638e5f80273f1bb7
dixieflatline76 <karl.kwong@gmail.com>
2024-12-02 17:02:38 +0100

    added feat last n connected countries and pref ui

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ import "@girs/glib-2.0/ambient";
 import "@girs/gnome-shell/ambient";
 import '@girs/gnome-shell/ui/ambient';
 import "@girs/gobject-2.0/ambient";
+import "@girs/adw-1/ambient";
+import "@gris/gtk-4.0/ambient";
